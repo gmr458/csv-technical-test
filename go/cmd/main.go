99266a8bacfa5d9ee5d26f6270ba423ea7e974dd
German David <germanmarinrolong@gmail.com>
2024-04-27 22:20:22 -0500

    add go version

---
+++ go/cmd/main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"encoding/csv"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+)
+
+var globalData []map[string]string
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("POST /api/files", handlerFiles)
+	mux.HandleFunc("GET /api/users", handlerUsers)
+
+	log.Println("listening on port 3000")
+	err := http.ListenAndServe(":3000", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handlerFiles(w http.ResponseWriter, r *http.Request) {
+	r.Body = http.MaxBytesReader(w, r.Body, 10_485_760)
+
+	file, fileHeader, err := r.FormFile("file")
+	if err != nil {
+		log.Println(err)
+		sendJSON(w, http.StatusBadRequest, envelope{"message": "A file must be provided"}, nil)
+		return
+	}
+	defer file.Close()
+
+	if fileHeader.Header.Get("Content-Type") != "text/csv" {
+		sendJSON(w, http.StatusBadRequest, envelope{"message": "The file type must be CSV"}, nil)
+		return
+	}
+
+	reader := csv.NewReader(file)
+	keys, err := reader.Read()
+	if err != nil {
+		if err == io.EOF {
+			sendJSON(w, http.StatusBadRequest, envelope{"message": "The file must not be empty"}, nil)
+			return
+		}
+
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Println(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		item := make(map[string]string)
+
+		for index, key := range keys {
+			item[key] = record[index]
+		}
+
+		globalData = append(globalData, item)
+	}
+
+	if len(globalData) == 0 {
+		sendJSON(w, http.StatusBadRequest, envelope{
+			"message": "Send a file with records",
+		}, nil)
+		return
+	}
+
+	sendJSON(w, http.StatusOK, envelope{
+		"message": "File uploaded successfully",
+	}, nil)
+}
+
+func handlerUsers(w http.ResponseWriter, r *http.Request) {
+	if len(globalData) == 0 {
+		sendJSON(w, http.StatusNotFound, envelope{
+			"message": "There is not data, upload a CSV file first",
+		}, nil)
+		return
+	}
+
+	q := r.URL.Query().Get("q")
+	if q == "" {
+		sendJSON(w, http.StatusOK, envelope{
+			"data": globalData,
+		}, nil)
+		return
+	}
+
+	q = strings.ToLower(q)
+	var coincidences []map[string]string
+
+	for _, record := range globalData {
+		for _, v := range record {
+			if strings.Index(strings.ToLower(v), q) != -1 {
+				coincidences = append(coincidences, record)
+			}
+		}
+	}
+
+	sendJSON(w, http.StatusOK, envelope{
+		"data": coincidences,
+	}, nil)
+}
---
+++ go/go.mod
@@ -0,0 +1,3 @@
+module github.com/gmr458/csv-technical-test-go
+
+go 1.22.2
