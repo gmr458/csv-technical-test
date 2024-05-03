4c6ae5d39a8429acf825d064e192c465c60c380f
German David <germanmarinrolong@gmail.com>
2024-05-02 19:49:40 -0500

    all in one file, remove unused function, add sync.Mutex, better logging and response error

--- go/cmd/main.go
+++ go/cmd/main.go
@@ -2,40 +2,101 @@ package main
 
 import (
 	"encoding/csv"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
 	"io"
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 	"strings"
+	"sync"
 )
 
-var globalData []map[string]string
-
 func main() {
+	var port int
+	flag.IntVar(&port, "port", 3000, "HTTP Port")
+	flag.Parse()
+
+	app := newApp(port)
+
 	mux := http.NewServeMux()
+	mux.HandleFunc("POST /api/files", app.handlerFiles)
+	mux.HandleFunc("GET /api/users", app.handlerUsers)
 
-	mux.HandleFunc("POST /api/files", handlerFiles)
-	mux.HandleFunc("GET /api/users", handlerUsers)
+	app.serve(mux)
+}
 
-	log.Println("listening on port 3000")
-	err := http.ListenAndServe(":3000", mux)
+type app struct {
+	data   []map[string]string
+	logger *slog.Logger
+	mu     sync.Mutex
+	port   int
+}
+
+func (app *app) serve(mux *http.ServeMux) {
+	app.logger.Info("listening", "port", app.port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", app.port), mux)
 	if err != nil {
-		log.Fatal(err)
+		app.logger.Error(err.Error())
+		os.Exit(1)
 	}
 }
 
-func handlerFiles(w http.ResponseWriter, r *http.Request) {
+func newApp(port int) *app {
+	return &app{
+		data:   []map[string]string{},
+		logger: slog.New(slog.NewJSONHandler(os.Stdout, nil)),
+		port:   port,
+	}
+}
+
+func (app *app) responseError(w http.ResponseWriter, err error) {
+	app.logger.Error(err.Error())
+	code := http.StatusInternalServerError
+	http.Error(w, http.StatusText(code), code)
+}
+
+type envelope map[string]any
+
+func (app *app) sendJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) {
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.responseError(w, err)
+		return
+	}
+
+	js = append(js, '\n')
+
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	if _, err := w.Write(js); err != nil {
+		app.responseError(w, err)
+	}
+}
+
+func (app *app) handlerFiles(w http.ResponseWriter, r *http.Request) {
 	r.Body = http.MaxBytesReader(w, r.Body, 10_485_760)
 
 	file, fileHeader, err := r.FormFile("file")
 	if err != nil {
-		log.Println(err)
-		sendJSON(w, http.StatusBadRequest, envelope{"message": "A file must be provided"}, nil)
+		if errors.Is(err, io.EOF) {
+			app.sendJSON(w, http.StatusBadRequest, envelope{"message": "A file must be provided"}, nil)
+			return
+		}
+
+		app.responseError(w, err)
 		return
 	}
 	defer file.Close()
 
 	if fileHeader.Header.Get("Content-Type") != "text/csv" {
-		sendJSON(w, http.StatusBadRequest, envelope{"message": "The file type must be CSV"}, nil)
+		app.sendJSON(w, http.StatusBadRequest, envelope{"message": "The file type must be CSV"}, nil)
 		return
 	}
 
@@ -43,29 +104,30 @@ func handlerFiles(w http.ResponseWriter, r *http.Request) {
 	keys, err := reader.Read()
 	if err != nil {
 		if err == io.EOF {
-			sendJSON(w, http.StatusBadRequest, envelope{"message": "The file must not be empty"}, nil)
+			app.sendJSON(w, http.StatusBadRequest, envelope{"message": "The file must not be empty"}, nil)
 			return
 		}
 
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		app.responseError(w, err)
 		return
 	}
 
 	rows, err := reader.ReadAll()
 	if err != nil {
-		log.Println(err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		app.responseError(w, err)
 		return
 	}
 	if len(rows) == 0 {
-		sendJSON(w, http.StatusBadRequest, envelope{
+		app.sendJSON(w, http.StatusBadRequest, envelope{
 			"message": "Send a file with records",
 		}, nil)
 		return
 	}
 
-	if len(globalData) > 0 {
-		globalData = nil
+	app.mu.Lock()
+
+	if len(app.data) > 0 {
+		app.data = nil
 	}
 
 	for _, cell := range rows {
@@ -75,17 +137,22 @@ func handlerFiles(w http.ResponseWriter, r *http.Request) {
 			item[key] = cell[index]
 		}
 
-		globalData = append(globalData, item)
+		app.data = append(app.data, item)
 	}
 
-	sendJSON(w, http.StatusOK, envelope{
+	app.mu.Unlock()
+
+	app.sendJSON(w, http.StatusOK, envelope{
 		"message": "File uploaded successfully",
 	}, nil)
 }
 
-func handlerUsers(w http.ResponseWriter, r *http.Request) {
-	if len(globalData) == 0 {
-		sendJSON(w, http.StatusNotFound, envelope{
+func (app *app) handlerUsers(w http.ResponseWriter, r *http.Request) {
+	app.mu.Lock()
+	defer app.mu.Unlock()
+
+	if len(app.data) == 0 {
+		app.sendJSON(w, http.StatusNotFound, envelope{
 			"message": "There is not data, upload a CSV file first",
 		}, nil)
 		return
@@ -93,8 +160,8 @@ func handlerUsers(w http.ResponseWriter, r *http.Request) {
 
 	q := r.URL.Query().Get("q")
 	if q == "" {
-		sendJSON(w, http.StatusOK, envelope{
-			"data": globalData,
+		app.sendJSON(w, http.StatusOK, envelope{
+			"data": app.data,
 		}, nil)
 		return
 	}
@@ -102,7 +169,7 @@ func handlerUsers(w http.ResponseWriter, r *http.Request) {
 	q = strings.ToLower(q)
 	var coincidences []map[string]string
 
-	for _, record := range globalData {
+	for _, record := range app.data {
 		for _, v := range record {
 			if strings.Index(strings.ToLower(v), q) != -1 {
 				coincidences = append(coincidences, record)
@@ -110,7 +177,7 @@ func handlerUsers(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	sendJSON(w, http.StatusOK, envelope{
+	app.sendJSON(w, http.StatusOK, envelope{
 		"data": coincidences,
 	}, nil)
 }
